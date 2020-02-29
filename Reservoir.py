35ed2fca1496f1191abcdaaa7766c1f26f843514
chasepratt <cwprattiv@gmail.com>
2020-02-28 22:39:43 -0500

    Reservoir is now trainable and works

--- Reservoir.py
+++ Reservoir.py
@@ -5,8 +5,9 @@ import scipy.sparse
 import sys
 
 class Reservoir:
-    def __init__(self, in_dim, res_dim, out_dim, timeConst=1, density=0.1, biasScale=1.0):
-        """The initialization method for a Reservoir object.
+    def __init__(self, in_dim, res_dim, out_dim, timeConst=1, density=0.1, biasScale=1.0, SR = 1.0):
+        """
+        The initialization method for a Reservoir object.
 
         Args:
             in_dim (int): The amount of inputs to the reservoir
@@ -22,6 +23,7 @@ class Reservoir:
         self.timeConst = timeConst
         self.density = density
         self.biasScale = biasScale
+        self.SR = SR
         self.output = 0
 
         self.inWeights = np.random.uniform(low=-1, high=1, size=(res_dim, in_dim))
@@ -29,14 +31,17 @@ class Reservoir:
         rvs = scipy.stats.norm().rvs
         W = scipy.sparse.random(m=res_dim, n=res_dim, density=density, data_rvs=rvs)
         W_rho = max(abs(np.linalg.eig(W.A)[0]))
-        self.matrix = W.A/W_rho
+        self.matrix = self.SR*W.A/W_rho
 
         self.bias = biasScale*np.random.uniform(low=-1, high=1, size=(res_dim))
 
         self.outWeights = np.zeros((out_dim, res_dim))
+        self.times = np.zeros(1)
+        self.state = np.zeros((1,res_dim))
 
-    def train(self, inputs, targets):
-        """Trains the reservoir by changing self.outWeights
+    def train(self, targets, initTime = 0):
+        """
+        Trains the reservoir by changing self.outWeights
 
         Changes self.outWeights
 
@@ -44,20 +49,27 @@ class Reservoir:
             inputs (matrix): The set of inputs that will be used for training
             targets (matrix): The set of targets training should aim for
         """
-        inputsTransposed = inputs.T
-        self.outWeights = np.dot(np.dot(targets.T, inputs), np.linalg.inv(np.dot(inputsTransposed, inputs) + 1e-6*np.eye(self.res_dim)))
+        tempTime = targets.shape[0]
+        X = self.state[initTime:initTime + tempTime]
+        #print(X)
+        X_T = X.T
+        self.outWeights = np.dot(np.dot(targets.T, X), np.linalg.inv(np.dot(X_T, X) + 1e-6*np.eye(self.res_dim)))
         def g_res(x, t):
             noise_g = 0
             return np.dot(self.outWeights, x.T).T + noise_g*np.random.normal(size=(self.out_dim))
-        self.outputs = g_res(inputs, 0)
+        self.outputs = g_res(self.state, 0)
 
-    def getVals(self, inputs, currentOutputs):
-        """For use once the reservoir is trained. Gives outputs based off the inputs
+    def getVals(self, inputs, timeStep = .1):
+        """
+        For use once the reservoir is trained. Gives outputs based off the inputs
         """
 
         def f_res(x, u, t):
             c = 1
             #noise_f = 0
+            if x.shape == (self.res_dim,):
+                x = x.reshape(self.res_dim,1)
+            #print(x.shape)
             temp1 = (-1/c)*x + (1/c)*np.tanh(np.dot(self.inWeights, u) + np.dot(self.matrix, x) + self.bias.reshape(50,1)) #+ noise_f*np.random.normal(size=(self.res_dim))
             #print(temp1.shape)
             return temp1
@@ -67,7 +79,8 @@ class Reservoir:
             return np.dot(self.outWeights, x.T).T + noise_g*np.random.normal(size=(self.out_dim))
 
         def runge_kutta_4(f, h, x, u, t):
-            """Function for solving a differential equation (with input) 
+            """
+            Function for solving a differential equation (with input) 
             according to a 4th-order Runge-Kutta method.
 
             Args:
@@ -96,13 +109,16 @@ class Reservoir:
 
             return x + (h/6)*(k1(f, h, x, u, t) + 2*k2(f, h, x, u, t) + 2*k3(f, h, x, u, t) + k4(f, h, x, u, t))
 
-        newOutputs = runge_kutta_4(f_res, 1, currentOutputs, inputs, 1)
-        newOutputs = newOutputs.reshape(1, self.res_dim)
-        self.output = g_res(newOutputs, 1)
+        propagations = runge_kutta_4(f_res, timeStep, self.state[-1].reshape(50,1), inputs, self.times[-1])
+        propagations = propagations.reshape(1, self.res_dim)
+        self.state = np.append(self.state, propagations, axis=0)
+        self.output = g_res(propagations, 1)
+        self.times = np.append(self.times, self.times[-1]+timeStep)
         return self.output
 
     def exportReservoir(self):
-        """Exports the reservoir
+        """
+        Exports the reservoir
         """
         #TODO figure out format of export
         #temp line to make IDE happy
--- ReservoirTest.py
+++ ReservoirTest.py
@@ -1,54 +1,48 @@
 import Reservoir
 import numpy as np
 
-def factoryFunc(temp):
-    return temp * 100
-
-wanted = 150
-wantedOutput = np.array([[50]])
-currentOutputs = np.array([[100]])
-inputMatrix = np.array([[1]])
-for x in range(49):
-    wantedOutput = np.append(wantedOutput, [[50]])
-    currentOutputs = np.append(currentOutputs, [[100]])
-    inputMatrix = np.append(inputMatrix, [[1]])
-input = 1
+wanted = 50
 inputM = np.array([[1]])
 outputM = np.array([[50]])
 outputM = outputM.reshape(1,1)
 output = 0
 
-wantedOutput = wantedOutput.reshape(50, 1)
-currentOutputs = currentOutputs.reshape(50, 1)
-inputMatrix = inputMatrix.reshape(50, 1)
-inputMatrix = inputMatrix.T
-wantedOutput = wantedOutput.T
-
-
-for x in range(10):
-    output = factoryFunc(input)
-    print("input: ", input)
-    print("output: ", output)
-    if output > wanted:
-        input = 1
-    else :
-        input = 2
 res = Reservoir.Reservoir(1, 50, 1)
-res.train(inputMatrix, outputM)
-#output = res.getVals(inputM, currentOutputs)
-for x in range(10):
-    output = res.getVals(inputM, currentOutputs)
-    currentOutputs = np.array(output)
-    for x in range(49):
-        currentOutputs = np.append(currentOutputs, output)
-    currentOutputs = currentOutputs.reshape(50, 1)
-    print("input: ", input)
+res.train(outputM, initTime=0)
+output = res.getVals(inputM)
+outputM = np.append(outputM, [[50]])
+outputM = outputM.reshape(-1, 1)
+print("input: ", inputM)
+print("output: ", output[0][0])
+while (output[0][0] > (wanted * 1.05)) | (output[0][0] < (wanted * .95)):
+    res.train(outputM)
+    output = res.getVals(inputM)
+    outputM = np.append(outputM, [[50]])
+    outputM = outputM.reshape(-1, 1)
+    print("input: ", inputM)
     print("output: ", output[0][0])
-    if (output[0][0] + 100) != wanted:
-        temp = 50 - output[0][0]
-        temp = np.array(temp)
-        temp = temp.reshape(1, 1)
-        res.train(inputMatrix, outputM)
-        print(res.outWeights)
-    else :
-        break
+
+output = res.getVals(inputM + .001)
+print("input: ", inputM+ .001)
+print("output: ", output[0][0])
+output = res.getVals(inputM + .01)
+print("input: ", inputM+ .01)
+print("output: ", output[0][0])
+output = res.getVals(inputM + .1)
+print("input: ", inputM + .1)
+print("output: ", output[0][0])
+output = res.getVals(inputM + 1)
+print("input: ", inputM + 1)
+print("output: ", output[0][0])
+output = res.getVals(inputM - .001)
+print("input: ", inputM - .001)
+print("output: ", output[0][0])
+output = res.getVals(inputM - .01)
+print("input: ", inputM - .01)
+print("output: ", output[0][0])
+output = res.getVals(inputM - .1)
+print("input: ", inputM - .1)
+print("output: ", output[0][0])
+output = res.getVals(inputM - 1)
+print("input: ", inputM - 1)
+print("output: ", output[0][0])
