f9543ad32add12d4e5a828c681e324295cd60cf6
chasepratt <cwprattiv@gmail.com>
2020-04-14 16:33:36 -0400

    Refactor the reservoir file and add more comments

--- ReservoirTestSinWave.py
+++ ReservoirTestSinWave.py
@@ -3,19 +3,27 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
+"""
+This program uses the Reservoir class from Reservoir.py to create a sin wave.
+The input to the reservoir for this test is a sin wave.
+The expected output of the reservoir is a sin wave with twice the amplitude of the input sin wave
+"""
 totalTime = 300
 inputM = []
 wantedOutputM = []
 outputM = []
 linespace = np.linspace(0,1,totalTime)
 
-res = Reservoir.Reservoir(1, 50, 1)
+# Changing the tuning parameters here is a good way to see how they influence the reservoir.
+# If the size of the reservoir gets much larger than 3 then the reservoir is too good and doesn't
+# display changes in the parameters too well.
+res = Reservoir.Reservoir(1, 3, 1, SR = 1.1, density = .5, biasScale= .5, reg = 1e-6)
 
 #Training
 for x in linespace:
     if inputM == []:
         inputM = np.array(.5*math.sin(8*3.14*x)).reshape(1,1)
-        wantedOutputM = np.array(math.sin(8*314*x)).reshape(1,1)
+        wantedOutputM = np.array(math.sin(8*3.14*x)).reshape(1,1)
     else:
         inputM = np.append(inputM, np.array(.5*math.sin(8*3.14*x)).reshape(1,1), axis = 0)
         wantedOutputM = np.append(wantedOutputM, np.array(math.sin(8*3.14*x)).reshape(1,1), axis = 0)
